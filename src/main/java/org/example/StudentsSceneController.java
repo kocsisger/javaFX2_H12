9238bb2d4425a0b36d127bd2d32b3d5931259f68
Minden Áron <you@inf.unideb.hu>
2024-09-30 13:16:13 +0200

    Load data works

--- src/main/java/org/example/StudentsSceneController.java
+++ src/main/java/org/example/StudentsSceneController.java
@@ -3,11 +3,30 @@ package org.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.example.model.Model;
 
 public class StudentsSceneController {
 
+    private Model model = new Model();
+
     @FXML
     private Label colorLabel;
+
+    @FXML
+    private Label creditsLabel;
+
+    @FXML
+    private Label dateOfBirthLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    void loadData(ActionEvent event) {
+        nameLabel.setText(model.getStudent().getName());
+        creditsLabel.setText("" + model.getStudent().getCredits());
+        dateOfBirthLabel.setText(model.getStudent().getDateOfBirth().toString());
+    }
 
     @FXML
     void changeColor(ActionEvent event) {
